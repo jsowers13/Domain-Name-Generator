7e63e4a2b6e08b0b7f39be2f8de4e31848f0b376
jsowers13 <jsowers13@gmail.com>
2022-03-25 23:22:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = () => {
+//   document.querySelector("#domain-btn").addEventListener("click", () => {
+//     document.querySelector("#the-domain").innerHTML = generateDomainNames(
+//       pronoun,
+//       adj
+//     );
+//   });
+// };
+
+window.onload = () => {
+  document.getElementById("the-domain").innerHTML = bigArray3.join("<br>");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".edu", ".org"];
+
+function combineArrays(array1, array2) {
+  var combinationArray = array1.flatMap(d => array2.map(v => d + v));
+  return combinationArray;
+}
+let bigArray1 = combineArrays(pronoun, adj);
+let bigArray2 = combineArrays(bigArray1, noun);
+let bigArray3 = combineArrays(bigArray2, ext);
+
+// let ext = [".com", ".edu", ".org"];
+
+// let domainList = [];
+
+// for (let i = 0; i < pronoun.length; i++) {
+//   domainList.push(pronoun[i] + adj[i] + noun[i] + ".com");
+//   domainList.push(pronoun[i] + adj[i] + noun[i + 1] + ".com");
+//   domainList.push(pronoun[i - 1] + adj[i] + noun[i] + ".com");
+// }
+// for (let i = 0; i < domainList.length; i++) {
+//   var newArray = domainList.slice(0, 1);
+// }
+// return newArray;
+// return domainList;
